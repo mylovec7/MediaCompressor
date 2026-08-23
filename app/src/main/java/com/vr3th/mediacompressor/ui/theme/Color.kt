f007c4167ef659c5d4190ad1917d89524ae763a9
mylovec7 <siskaaa448@gmail.com>
2026-08-23 21:53:34 +0700

    Delete app/src/main/java/com/vr3th/mediacompressor/ui/theme/Color.kt

--- app/src/main/java/com/vr3th/mediacompressor/ui/theme/Color.kt
+++
@@ -1,4 +0,0 @@
-package com.vr3th.mediacompressor.ui.theme
-import androidx.compose.ui.graphics.Color
-val CharcoalBlack=Color(0xFF121214);val CharcoalSurface=Color(0xFF1A1A1E);val CharcoalCard=Color(0xFF24242A);val CharcoalBorder=Color(0xFF33333C)
-val DustyPink=Color(0xFFDDA7A5);val DustyPinkDark=Color(0xFFC88A8A);val DustyPinkContainer=Color(0x33DDA7A5);val OffWhite=Color(0xFFF5F5F7);val TextMuted=Color(0xFF9E9EA7);val SuccessGreen=Color(0xFF81C784);val ErrorRed=Color(0xFFE57373)
